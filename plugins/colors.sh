978ff5c5b6c25c6f398ce42ae37eb034b5e0fb54
bjarneo <bjarne.oeverli@gmail.com>
2025-07-05 09:29:06 +0200

    fix: add color to ag

--- plugins/colors.sh
+++ plugins/colors.sh
@@ -3,6 +3,7 @@ alias ls='ls --color=auto'
 alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
+alias ag='ag --color'
 
 # Check if the dircolors command is available
 if command -v dircolors >/dev/null; then
